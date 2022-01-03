5119ea2064def65b39db378ba34461e2ec80186c
Anders Børjesson <anbo@zealand.dk>
2022-01-03 20:33:27 +0100

    Add project files.

---
+++ RESTresale/Managers/SalesItemContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTresale.Managers
+{
+    public class SalesItemContext : DbContext
+    {
+        public SalesItemContext
+            (DbContextOptions<SalesItemContext> options) : base(options) { }
+
+        public DbSet<SalesItemContext> SalesItems { get; set; }
+    }
+}
---
+++ RESTresale/Models/ResaleItem.cs
@@ -0,0 +1,18 @@
+﻿namespace RESTresale.Models
+{
+    public class ResaleItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Seller { get; set; }
+        public int Date { get; set; }
+        public string PictureUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Title + " " + Price;
+        }
+    }
+}
